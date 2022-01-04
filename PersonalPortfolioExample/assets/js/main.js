462cfa0e8926666340e32e872a46c76eddb55ec7
Alexandre Santos <ale.santos49@gmail.com>
2022-01-04 15:01:12 -0300

    Finalizado as seções e iniciado o tratamentos das media queries

--- PersonalPortfolioExample/assets/js/main.js
+++ PersonalPortfolioExample/assets/js/main.js
@@ -1,19 +1,19 @@
 // == MENU SHOW AND HIDDEN ==
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 // MENU SHOW
 //valida se o conteudo existe
-if(navToggle){
-    navToggle.addEventListener('click',()=>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
 // MENU HIDDEN
-if(navClose){
-    navClose.addEventListener('click',()=>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -21,7 +21,7 @@ if(navClose){
 // REMOVE MENU MOBILE
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
 
     //Quando é clicado em algum nav__link, removemos a classe 'show-menu'
@@ -32,39 +32,39 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /* ACCORDION SKILLS */
 const skillsContent = document.getElementsByClassName('skills__content'),
-      skillsHeader = document.querySelectorAll('.skills__header')
+    skillsHeader = document.querySelectorAll('.skills__header')
 
-function toggleSkills(){
+function toggleSkills() {
     let itemClass = this.parentNode.className
 
-    for(i = 0; i<skillsContent.length;i++){
+    for (i = 0; i < skillsContent.length; i++) {
         skillsContent[i].className = 'skills__content skills__close'
     }
 
-    if(itemClass === 'skills__content skills__close'){
+    if (itemClass === 'skills__content skills__close') {
         this.parentNode.className = 'skills__content skills__open';
     }
 }
 
-skillsHeader.forEach((el) =>{
+skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
 
 // QUALIFICATION TABS 
 const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]');
+    tabContents = document.querySelectorAll('[data-content]');
 
 tabs.forEach(tab => {
-    tab.addEventListener('click', () =>{
+    tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.target);
 
-        tabContents.forEach(tabContent =>{
+        tabContents.forEach(tabContent => {
             tabContent.classList.remove('qualification__active')
         })
 
         target.classList.add('qualification__active')
 
-        tab.forEach(tab =>{
+        tab.forEach(tab => {
             tab.classList.remove('qualification__active')
         })
         tab.classList.add('qualification__active')
@@ -74,37 +74,131 @@ tabs.forEach(tab => {
 // SERVICES MODAL
 
 const modalViews = document.querySelectorAll('.services__modal'),
-      modalBtns = document.querySelectorAll('.services__button'),
-      modalCloses = document.querySelectorAll('.services__modal-close');
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close');
 
-let modal = function(modalClick){
+let modal = function (modalClick) {
     modalViews[modalClick].classList.add('active-modal')
 }
 
-modalBtns.forEach((modalBtn, i) =>{
-    modalBtn.addEventListener('click', ()=>{
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
         modal(i)
     })
 })
 
 modalCloses.forEach((modalClose) => {
-    modalClose.addEventListener('click',()=>{
-        modalViews.forEach((modalView)=>{
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
             modalView.classList.remove('active-modal')
         })
     })
 })
 
 // PORTFOLIO SWIPER
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true
+        el: ".swiper-pagination",
+        clickable: true
     },
-  });
+});
+
+//TESTIMONIAL SWIPER
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grapCursor: true,
+    spaceBetween: 48,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        }
+    }
+});
+
+// SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+// CHANGE BACKGROUND HEADER
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    //Quando o scroll é maior que 80 da altura da viewport, adiciona a classe scroll-header para a header tag
+    if (this.scrollY >= 80) {
+        nav.classList.add('scroll-header');
+    } else {
+        nav.classList.remove('scroll-header');
+    }
+}
+
+window.addEventListener('scroll',scrollHeader)
+
+//SHOW SCROLL UP
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    
+    if(this.scrollY >= 560){
+        scrollUp.classList.add('show-scroll');
+    }else{
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+window.addEventListener('scroll',scrollUp)
+
+// DARK LIGHT THEME
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// tópico previamente selecionado (caso usuário tenha selecionado)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectIcon = localStorage.getItem('selected-icon')
+
+//We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.constains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => document.body.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+//Ativa ou desativa manualmente o tema com o botão
+themeButton.addEventListener('click',()=>{
+    //Adiciona ou remove tema / icone dark
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    //Salvamos o tema e icone corrente que o user escolher
+    localStorage.setItem('selected-theme',getCurrentTheme())
+    localStorage.setItem('selected-icon',getCurrentIcon())
+})
+
